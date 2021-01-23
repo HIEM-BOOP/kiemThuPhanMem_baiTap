0b2c25f23f5df812702f819bb8c8c5d1482c8fe6
Hiêm <hiemkbr.gmail.com>
2021-01-23 14:48:38 +0700

    lab 3

--- 04-de-quy/js/javascript.js
+++ 04-de-quy/js/javascript.js
@@ -1,4 +1,39 @@
 // Ham de quy
-pow = (n,x) => {
-    
+pow = (n, x) => {
+    if (x === 1) {
+        return n;
+    } else {
+        return x * pow(n, x - 1);
+    }
 }
+console.log(pow(2, 1));
+// Tong 
+
+sum = (n) => {
+    if (n === 1) {
+        return n;
+    } else {
+        return sum(n - 1) + n
+    }
+}
+
+console.log(sum(5));
+
+factorial = (n) => {
+    if (n === 1) {
+        return n;
+    } else {
+        return sum(n - 1) * n
+    }
+}
+console.log(factorial(5))
+
+fibonacci = (n) => {
+    if(n <= 1){
+        return 1 ;
+    }else{
+        return fibonacci(n-1) + fibonacci(n-2) ;
+    }
+
+}
+console.log(fibonacci(5))
